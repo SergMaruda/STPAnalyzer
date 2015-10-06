0f619da2c06b642d82fecc1754f595ee7f048696
Sergey <maruda@ukr.net>
2015-10-07 01:40:23 +0300

    dumping surfaces centers

--- src/Main.cpp
+++ src/Main.cpp
@@ -28,18 +28,52 @@
 #include <BRepGProp.hxx>
 #include <StepGeom_Placement.hxx>
 #include <GeomTools_SurfaceSet.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <Precision.hxx>
 
 #include <set>
 #include <map>
 #include <vector>
 
 
+//-----------------------------------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& i_op, const gp_Pnt& i_pt)
+{
+	i_op<<"(" << i_pt.X() << "," << i_pt.Y() << ","<< i_pt.Z() << ")";
+	return i_op;
+}
+
+//-----------------------------------------------------------------------------------------------------
+gp_Pnt SurfaceCenter(const TopoDS_Shape& i_shape)
+{
+	GProp_GProps props;
+	BRepGProp::SurfaceProperties(i_shape, props);
+	return props.CentreOfMass();
+}
+
+//-----------------------------------------------------------------------------------------------------
+TopoDS_Shape ToShape(Handle(Geom_Surface) s) 
+{
+	const Standard_Real TolDegen = 1E-7;
+// 	Standard_Real u1,u2,v1,v2;
+// 	s->Bounds(u1,u2,v1,v2);
+	BRepBuilderAPI_MakeFace mkBuilder(s,/* u1, u2, v1, v2, */TolDegen);
+
+	return mkBuilder.Shape();
+}
+
 //------------------------
-void DumpSurfaceInfo(Handle(StepGeom_Surface) i_surface)
+void DumpSurfaceInfo(Handle(StepGeom_Surface) i_step_geom_surface)
 	{
-		Handle(Geom_Surface) res;
-		StepToGeom_MakeSurface::Convert(i_surface, res);
-		GeomTools_SurfaceSet::PrintSurface(res, std::cout);
+		Handle(Geom_Surface) geom_surf;
+		StepToGeom_MakeSurface::Convert(i_step_geom_surface, geom_surf);
+
+		auto shape = ToShape(geom_surf);
+
+		auto center = SurfaceCenter(shape );
+
+		GeomTools_SurfaceSet::PrintSurface(geom_surf, std::cout);
+		std::cout<<"Surface center: "<< center << "\n";
     }
 
 //------------------------
@@ -65,6 +99,8 @@ IFSelect_ReturnStatus DumpInfo(const Standard_CString& aFileName)
 	std::vector<Handle(StepGeom_Placement)> axes;
 
 	std::map<std::string, size_t> num_types;
+
+	std::cout<<"Analyzing... \n";
 
 	for(Standard_Integer i = 1; i <=num; ++i)
 	{
@@ -96,29 +132,36 @@ IFSelect_ReturnStatus DumpInfo(const Standard_CString& aFileName)
 			}
 		}
 
-		auto dn_t = ent->DynamicType();
-		diff_types.insert(dn_t->Name());
-		++num_types[dn_t->Name()];
 	}
 
-	std::cout<<"Number of surfaces: "<< surfaces.size() + cylinders.size() + planes.size() <<"\n";
-	std::cout<<"Number of planes: "<< planes.size()<<"\n";
-	std::cout<<"Number of cylindrical surface: "<< cylinders.size()<<"\n";
-    std::cout<<"Number of axes: "<< axes.size()<<"\n\n";
+	auto sufraces_num = surfaces.size() + cylinders.size() + planes.size();
 
-	std::cout<<"Dumping surfaces...\n\n";
+	std::cout<<"Number of surfaces found (including "<< planes.size() <<" plane(s), "<<cylinders.size()<<" cylindrical surface(s)):" << sufraces_num << "\n";
+    std::cout<<"Number of axes found: "<< axes.size()<<"\n\n";
 
+	std::cout<<"Dumping planes...\n";
 
-	for(Standard_Integer i = 1; i <=num; ++i)
+	for(size_t i = 0; i < planes.size(); ++i)
 	{
-		auto ent = model->Entity(i);
-		auto surf = Handle(StepGeom_Surface)::DownCast (ent );
-		if(surf)
-		{
-			DumpSurfaceInfo(surf);
-		}
-		
+		DumpSurfaceInfo(planes[i]);
 	}
+
+
+	std::cout<<"Dumping cylindrical surfaces...\n";
+
+	for(size_t i = 0; i < cylinders.size(); ++i)
+	{
+		DumpSurfaceInfo(cylinders[i]);
+	}
+
+
+	std::cout<<"Dumping other surfaces...\n";
+
+	for(size_t i = 0; i < surfaces.size(); ++i)
+	{
+		DumpSurfaceInfo(surfaces[i]);
+	}
+
 
 
 	return status;
